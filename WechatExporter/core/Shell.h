f3af2662ae10aa8f24f0db802e578b9bf6553583
Matthew <37573096+BlueMatthew@users.noreply.github.com>
2021-01-21 18:22:35 +0800

    Remove useless code

--- WechatExporter/core/Shell.h
+++ WechatExporter/core/Shell.h
@@ -26,9 +26,6 @@ public:
 	virtual bool listSubDirectories(const std::string& path, std::vector<std::string>& subDirectories) const = 0;
     virtual bool copyFile(const std::string& src, const std::string& dest, bool overwrite) const = 0;
     virtual bool moveFile(const std::string& src, const std::string& dest) const = 0;
-	virtual bool openOutputFile(std::ofstream& ofs, const std::string& fileName, std::ios_base::openmode mode = std::ios::out) const = 0;
-    // virtual bool convertPlist(const std::vector<unsigned char>& bplist, std::string& xml) const = 0;
-    // virtual int exec(const std::string& cmd) const = 0;
     // ref: https://blackbeltreview.wordpress.com/2015/01/27/illegal-filename-characters-on-windows-vs-mac-os/
     virtual std::string removeInvalidCharsForFileName(const std::string& fileName) const = 0;
     virtual ~Shell() {}
