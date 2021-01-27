ea278759bdafea697a4b7c9fd705a2856da2b7db
Matthew <37573096+BlueMatthew@users.noreply.github.com>
2021-01-27 14:36:31 +0800

    Support for tag pair lookup

--- WechatExporter/core/ByteArrayLocater.h
+++ WechatExporter/core/ByteArrayLocater.h
@@ -30,6 +30,46 @@ public:
 
         return positions;
     }
+    
+    std::vector<std::pair<int, int>> locatePair(const unsigned char* data, int length, const unsigned char* startCandidate, int startCandidateLength, const unsigned char* endCandidate, int endCandidateLength)
+    {
+        std::vector<std::pair<int, int>> positions;
+        if (isEmptyLocate(data, length, startCandidate, startCandidateLength))
+            return positions;
+
+        for (int i = 0; i < length;)
+        {
+            if (!isMatch(data, length, i, startCandidate, startCandidateLength))
+            {
+                i++;
+                continue;
+            }
+            
+            int j = i + startCandidateLength;
+            
+            for (; j < length;)
+            {
+                if (!isMatch(data, length, j, endCandidate, endCandidateLength))
+                {
+                    j++;
+                    continue;
+                }
+                
+                positions.push_back(std::make_pair(i, j));
+                break;
+            }
+            
+            if (j == length)
+            {
+                // No endTag found
+                break;
+            }
+            
+            i = j + endCandidateLength;
+        }
+
+        return positions;
+    }
 
     bool isMatch(const unsigned char* data, int length, int position, const unsigned char* candidate, int candidateLength)
     {
