1f51b742819ebae8c60ebd519536ae94277fb215
Matthew <37573096+BlueMatthew@users.noreply.github.com>
2021-08-05 22:25:16 +0800

    Fix displayName for wechat user

--- WechatExporter/core/Exporter.cpp
+++ WechatExporter/core/Exporter.cpp
@@ -720,7 +720,7 @@ bool Exporter::loadUserFriendsAndSessions(const Friend& user, Friends& friends, 
     std::string uidMd5 = user.getHash();
     std::string userBase = combinePath("Documents", uidMd5);
     
-    if (detailedInfo)
+    // if (detailedInfo)
     {
         std::string wcdbPath = m_iTunesDb->findRealPath(combinePath(userBase, "DB", "WCDB_Contact.sqlite"));
         FriendsParser friendsParser(detailedInfo);
